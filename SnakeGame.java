2b4eefc0a93119863bdb3b658439583b7599ef28
shero0o <shiyar.borgegg20@gmail.com>
2024-05-09 11:16:29 +0200

    fristcommmit

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,5 +1,6 @@
-public class App {
+public class SnakeGame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        new GameFrame();
     }
 }
